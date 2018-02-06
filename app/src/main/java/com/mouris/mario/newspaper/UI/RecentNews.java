5f2e1c6956d10700911d097683f306803402676b
Mario Amgad <marioamgad9@gmail.com>
2018-02-06 12:10:00 +0200

    Design article item

---
+++ app/src/main/java/com/mouris/mario/newspaper/UI/RecentNews.java
@@ -0,0 +1,31 @@
+package com.mouris.mario.newspaper.UI;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mouris.mario.newspaper.R;
+
+public class RecentNews extends Fragment {
+
+    public RecentNews() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recent_news, container, false);
+    }
+
+
+}
