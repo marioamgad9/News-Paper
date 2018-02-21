3384b2855ac6762839d77588461d0f981e319998
Mario Amgad <marioamgad9@gmail.com>
2018-02-21 17:27:39 +0200

    Add launch_screen

--- app/src/main/java/com/mouris/mario/newspaper/UI/MainActivity.java
+++ app/src/main/java/com/mouris/mario/newspaper/UI/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setElevation(0);
