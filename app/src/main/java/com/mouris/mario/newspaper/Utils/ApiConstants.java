c65b3c835527c9193765df351026b9a15505865e
Mario Amgad <marioamgad9@gmail.com>
2018-02-21 17:40:46 +0200

    Remove api key from ApiConstants

--- app/src/main/java/com/mouris/mario/newspaper/Utils/ApiConstants.java
+++ app/src/main/java/com/mouris/mario/newspaper/Utils/ApiConstants.java
@@ -1,31 +1,29 @@
 package com.mouris.mario.newspaper.Utils;
 
 
- class ApiConstants {
+import com.mouris.mario.newspaper.BuildConfig;
+
+class ApiConstants {
 
      static class UrlConstants {
         static final String SCHEMA = "https";
         static final String AUTHORITY = "newsapi.org";
         static final String API_PATH = "v2";
         static final String TOP_HEADLINES_PATH = "top-headlines";
-        static final String EVERYTHING_PATH = "everything";
     }
 
      static class ApiParameterKeys {
         static final String API_KEY = "apiKey";
          static final String COUNTRY = "country";
          static final String CATEGORY = "category";
-         static final String QUERY = "q";
     }
 
      static class ApiParameterValues {
-         static final String API_KEY = "e592cbb6575e4f0d9c1786d8c4c83d67";
+         static final String API_KEY = BuildConfig.API_KEY;
          static final String COUNTRY_US = "us";
-         static final String COUNTRY_EG = "eg";
     }
 
      static class ApiResponseKeys {
-         static final String STATUS = "status";
          static final String ARTICLES = "articles";
          static final String TITLE = "title";
          static final String AUTHOR = "author";
